78e619b4b796fc132018aa4b2290c4a0939329fc
webmarkyn <web.markyn@gmail.com>
2017-04-23 14:00:08 +0300

    added loop

--- 01_helloWorld/main.go
+++ 01_helloWorld/main.go
@@ -5,5 +5,8 @@ import (
 )
 
 func main() {
-	fmt.Println("Hello world")
+	// fmt.Printf("%d \t %b \t %#X \n", 42, 42, 42)
+	for i := 0; i <= 200; i++ {
+		fmt.Printf("%d \t %b \t %#X \t %q \n", i, i, i, i)
+	}
 }
